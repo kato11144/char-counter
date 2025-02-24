f5d586f0410c1b9b7d1f75b2c2ed2350c3c06498
kato11144 <ts4.29r@gmail.com>
2025-02-24 21:11:08 +0900

    update app.js

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-document.getElementById('text-box').addEventListener('input', function() {
-    const textWithoutNewlines = this.value.replace(/[\r\n]/g, '');
-    const charCount = textWithoutNewlines.length;
+function updateCharCount() {
+    const inputText = document.getElementById('text-box').value;
+    const cleanedText = inputText.replace(/[\r\n\t]/g, '');
+    const charCount = cleanedText.length;
     document.getElementById('char-count').textContent = charCount;
-});
+}
+
+function handleTabInput(event) {
+    if (event.key === 'Tab') {
+        event.preventDefault();
+        const textBox = document.getElementById('text-box');
+        const { selectionStart, selectionEnd, value } = textBox;
+        textBox.value = value.substring(0, selectionStart) + '\t' + value.substring(selectionEnd);
+        textBox.selectionStart = textBox.selectionEnd = selectionStart + 1;
+    }
+}
+
+const textBox = document.getElementById('text-box');
+textBox.addEventListener('input', updateCharCount);
+textBox.addEventListener('keydown', handleTabInput);
